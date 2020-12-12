3d93d1cc5ce317722c62ba18a7081779239b1a14
Nikita Shilnikov <fg@flashgordon.ru>
2020-12-12 19:21:40 +0300

    Make prototype-rails compatible with rails 5.1
    This removes unused test helpers. Use at your own risk (I sincerely hope you won't need it)

--- lib/prototype-rails.rb
+++ lib/prototype-rails.rb
@@ -3,15 +3,12 @@ require 'active_support'
 
 module PrototypeRails
   class Engine < Rails::Engine
-    initializer 'prototype-rails.initialize' do
-      ActiveSupport.on_load(:action_controller) do
-        require 'prototype-rails/on_load_action_controller'
-      end
+    ActiveSupport.on_load(:action_controller) do
+      require 'prototype-rails/on_load_action_controller'
+    end
 
-      ActiveSupport.on_load(:action_view) do
-        require 'prototype-rails/on_load_action_view'
-      end
+    ActiveSupport.on_load(:action_view) do
+      require 'prototype-rails/on_load_action_view'
     end
   end
 end
-
--- lib/prototype-rails/rendering.rb
+++ lib/prototype-rails/rendering.rb
@@ -1,13 +1,11 @@
 require 'action_view/helpers/rendering_helper'
 
-ActionView::Helpers::RenderingHelper.module_eval do
-  def render_with_update(options = {}, locals = {}, &block)
+ActionView::Helpers::RenderingHelper.prepend(Module.new {
+  def render(options = {}, locals = {}, &block)
     if options == :update
       update_page(&block)
     else
-      render_without_update(options, locals, &block)
+      super(options, locals, &block)
     end
   end
-  
-  alias_method_chain :render, :update
-end
+})
